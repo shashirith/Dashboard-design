795a7d95432be820f208bd9780ea2e784f12810c
shashirith <shashirith@gmail.com>
2023-10-10 17:03:30 +0530

    updates

--- src/Firebase.config.js
+++ src/Firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpITqP_bkoMcY4XzIM3Ratop9a1h_qIa4",
-  authDomain: "openinapp-a7f76.firebaseapp.com",
-  projectId: "openinapp-a7f76",
-  storageBucket: "openinapp-a7f76.appspot.com",
-  messagingSenderId: "947037521817",
-  appId: "1:947037521817:web:e16dfabee04fe496e03508"
+  apiKey: "AIzaSyCxpqOX8vnAD6nXNdpuxgonXKHElyregvQ",
+  authDomain: "dashboard-75c22.firebaseapp.com",
+  projectId: "dashboard-75c22",
+  storageBucket: "dashboard-75c22.appspot.com",
+  messagingSenderId: "1044940754620",
+  appId: "1:1044940754620:web:7fb1a6a9f5437dd0050ab9"
 };
 
 const app = initializeApp(firebaseConfig)
